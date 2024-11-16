a20f6b060d1ce33aba3dde3f31663991d64f4701
D13YSON <dieysonjason@gmail.com>
2024-11-15 21:15:56 -0300

    Configuração das variaveis de ambiente

--- index.js
+++ index.js
@@ -6,22 +6,16 @@ const { WebhookClient } = require('dialogflow-fulfillment');
 const path = require('path');
 const app = express();
 app.use(express.json());
-
-app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, 'index.html'));
-});
+require('dotenv').config();
 
 const calendar = google.calendar('v3');
 const timeZone = 'America/Sao_Paulo';
 const timeZoneOffset = '-03:00';
 const appointment_type = 'Entrega';
 
-const calendarId = "0b0a914a967a30a8e408d3a50af47beb3baf2439e321b0a6dffba12cb7a56c4c@group.calendar.google.com";
-const serviceAccount = {};
-
 const serviceAccountAuth = new google.auth.JWT({
-     email: serviceAccount.client_email,
-     key: serviceAccount.private_key,
+     email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+     key: process.env.GOOGLE_PRIVATE_KEY,
      scopes: 'https://www.googleapis.com/auth/calendar'
 });
 
@@ -72,7 +66,7 @@ function createCalendarEvent(dateTimeStart, dateTimeEnd, appointment_type) {
      return new Promise((resolve, reject) => {
           calendar.events.list({
                auth: serviceAccountAuth,
-               calendarId: calendarId,
+               calendarId: process.env.GOOGLE_CALENDAR_ID,
                timeMin: dateTimeStart.toISOString(),
                timeMax: dateTimeEnd.toISOString()
           }, (err, calendarResponse) => {
@@ -81,7 +75,7 @@ function createCalendarEvent(dateTimeStart, dateTimeEnd, appointment_type) {
                } else {
                     calendar.events.insert({
                          auth: serviceAccountAuth,
-                         calendarId: calendarId,
+                         calendarId: process.env.GOOGLE_CALENDAR_ID,
                          resource: {
                               summary: `${appointment_type}`,
                               start: { dateTime: dateTimeStart },
@@ -101,6 +95,10 @@ function createCalendarEvent(dateTimeStart, dateTimeEnd, appointment_type) {
 
 app.post('/webhook', (req, res) => {
      new DialogflowWebhook(req, res);
+});
+
+app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(8080, () => {
